4a8f5976caae6223c64f37ff24dfe494bdd38a3b
Darko <darkomarjanovickop@gmail.com>
2022-03-18 17:56:18 +0100

    +frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public function index() {
+        return view('home.userpage');
+    }
+
     public function redirect() {
         $usertype = Auth::user()->usertype;
 
